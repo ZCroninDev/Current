ceb2c8765c4c60da6cfc004c34e45c2c525156f0
zach <ZCroninDev@users.noreply.github.com>
2022-03-29 20:11:28 -0400

    Title data form api reponse

--- scripts.js
+++ scripts.js
@@ -2,11 +2,17 @@ const key = config.MY_API_Key;
 
 async function getArticles() {
     try {
-      const response = await axios.get('https://newsapi.org/v2/everything?q=technology&apiKey=' + key);
-      console.log(response);
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key);
+      let articlesTitle = response.data.articles[0].title;
+
+      console.log(articlesTitle)
     } catch (error) {
       console.error(error);
     }
   }
 
 getArticles()
+
+
+
+

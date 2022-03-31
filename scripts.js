2c0000b9e3830af447e835f0187935daea00eb98
zach <ZCroninDev@users.noreply.github.com>
2022-03-31 19:47:21 -0400

    Display title WIP

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 const key = config.MY_API_Key;
 
 async function getArticles() {
+  
     try {
       const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key);
       let articlesTitle = response.data.articles[0].title;
+      let title = document.getElementsByClassName('title');
+
+      for (let i=0; i < title.length; i++) {
+        
+      }
 
       console.log(articlesTitle)
     } catch (error) {

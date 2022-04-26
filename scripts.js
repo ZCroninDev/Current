d0bc72833c10d3c6a92fede1c2bc6f34a102fe04
zach <ZCroninDev@users.noreply.github.com>
2022-04-26 19:45:12 -0400

    Title display in html

--- scripts.js
+++ scripts.js
@@ -4,14 +4,17 @@ async function getArticles() {
   
     try {
       const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key);
-      let articlesTitle = response.data.articles[0].title;
-      let title = document.getElementsByClassName('title');
 
-      for (let i=0; i < title.length; i++) {
-        
+      
+      let articlesTitle = response.data.articles[0].title;
+      let titles = document.getElementsByClassName('title');
+
+      for (let i = 0; i < titles.length; i++) {
+        titles[i].innerHTML = articlesTitle;
       }
 
-      console.log(articlesTitle)
+
+      console.log()
     } catch (error) {
       console.error(error);
     }

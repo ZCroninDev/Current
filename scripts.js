1a6253a26cc7379e2bc4f0aa1db7e11d28a91890
zach <ZCroninDev@users.noreply.github.com>
2022-05-02 19:49:19 -0400

    Combining for loops

--- scripts.js
+++ scripts.js
@@ -8,22 +8,35 @@ async function getArticles() {
       // Gets article from api as html colection
       let articles = response.data.articles;
 
-      
+      // Gets html elements with the class .article-title 
+      let cardTitles = Array.from(document.getElementsByClassName('article-title'));
      
       // Converts html collection to an Array
       for (let i = 0; i < articles.length; i++) {
         articleTitles= Array(articles[i].title);
+        console.log(articleTitles);
+        
+        for (let j = 0; j < cardTitles.length; j++) {
+          cardTitles[j].innerHTML;
+        }
+
+        n.forEach((num1, index) => {
+          const num2 = m[index];
+          console.log(num1, num2);
+        });
+
       }
 
-      // Gets html elements with the class .article-title 
-      let cardTitles = document.getElementsByClassName('article-title');
+      
+      
 
       // loops through each cardTitle to display each article title
-      for (let i = 0; i < cardTitles.length; i++) {
-        cardTitles[i].innerHTML = articleTitles;
-      }
+      // for (let j = 0; j < cardTitles.length; j++) {
+      //   cardTitles[j].innerHTML = articleTitles;
+      // }
 
-      console.log(cardTitles)
+      console.log(cardTitles);
+      
 
     } catch (error) {
       console.error(error);
